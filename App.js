ca287acbfec0b0adf1dd2bf3a797419bedd3f3cf
emirdemiraslan <emir.demiraslan@gmail.com>
2017-11-20 17:15:27 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React from "react";
+import {Text, View} from "react-native";
+
+export default class App extends React.Component {
+
+  render(){
+    return (
+      <View>
+        <Text>Some Text</Text>
+        <Text>Some Text</Text>
+        <Text>Some Text</Text>
+        <Text>Some Text</Text>
+        <Text>Some Text</Text>
+      </View>
+
+    );
+  }
+}
